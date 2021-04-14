a9865f25712c98d233f8d4f37ad662858d9cf05c
KevennyJS <kevennykeke@gmail.com>
2021-04-14 19:55:19 -0300

    feat: corrigindo comportamento estranho da transição de tela do MTE

--- lib/template_questoes/multiple_choice.dart
+++ lib/template_questoes/multiple_choice.dart
@@ -52,34 +52,22 @@ class MultipleChoiceQuestion extends ConsumerWidget {
   }
 
   // Widget que retorna o quadro que representa o piece, seja ele uma imagem ou texto.
-  Widget piece(
-      int index,
-      BuildContext context,
-      ScopedReader watch,
-      Question question,
-      double buttonHeight,
-      double screenHeight,
-      double textCardHeight) {
+  Widget piece(int index, BuildContext context, ScopedReader watch, Question question, double buttonHeight, double screenHeight, double textCardHeight) {
     final buttonState = watch(buttonStateProvider).state;
 
     String grouping = (index + 1).toString();
     // double cardHeight = 158.29;
 
     // Define o tamanho do card com base nas dimensões do dispositivo, seguindo as proporções presentes no Figma.
-    double availableSpaceForCards =
-        screenHeight - textCardHeight - buttonHeight - 12 - 32;
+    double availableSpaceForCards = screenHeight - textCardHeight - buttonHeight - 12 - 32;
     double marginBetweenCards = 0.0147 * availableSpaceForCards;
-    double cardHeight =
-        (availableSpaceForCards - 24 - 2 * marginBetweenCards) / 3;
-    double cardWidth = question.pieces[grouping]["image"].isNotEmpty
-        ? cardHeight
-        : double.infinity;
+    double cardHeight = (availableSpaceForCards - 24 - 2 * marginBetweenCards) / 3;
+    double cardWidth = question.pieces[grouping]["image"].isNotEmpty ? cardHeight : double.infinity;
 
     bool audio = false;
 
     return Card(
-      margin: EdgeInsets.only(
-          bottom: index < 2 ? marginBetweenCards : 0, left: 12, right: 12),
+      margin: EdgeInsets.only(bottom: index < 2 ? marginBetweenCards : 0, left: 12, right: 12),
       clipBehavior: Clip.hardEdge,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
@@ -89,19 +77,13 @@ class MultipleChoiceQuestion extends ConsumerWidget {
         alignment: Alignment.bottomLeft,
         children: [
           ConstrainedBox(
-            constraints: BoxConstraints(
-                maxHeight: cardHeight,
-                maxWidth: cardWidth,
-                minHeight: cardHeight,
-                minWidth: cardWidth),
+            constraints: BoxConstraints(maxHeight: cardHeight, maxWidth: cardWidth, minHeight: cardHeight, minWidth: cardWidth),
             child: MaterialButton(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20),
                 side: BorderSide(
                   // Define a cor das bordas do card dependendo se ele estiver selecionado ou não.
-                  color: _buttonPressed[index]
-                      ? Color(0xFF00DC8C)
-                      : Color(0x6E729166),
+                  color: _buttonPressed[index] ? Color(0xFF00DC8C) : Color(0x6E729166),
                   width: 3,
                 ),
               ),
@@ -112,22 +94,21 @@ class MultipleChoiceQuestion extends ConsumerWidget {
                 tag: grouping,
                 child: question.pieces[grouping]["image"].isNotEmpty
                     ? Image.network(
-                        BASE_URL +
-                            '/image/' +
-                            question.pieces[grouping]["image"],
+                        BASE_URL + '/image/' + question.pieces[grouping]["image"],
                       )
                     // ? Image.asset('assets/img/placeholder.jpg')
-                    : Container(
-                        height: cardHeight,
-                        child: Center(
-                          child: Text(
-                            question.pieces[grouping]["text"].toUpperCase(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800,
-                                fontSize: fonteDaLetra),
+                    : Material(
+                        type: MaterialType.transparency,
+                        child: Container(
+                          height: cardHeight,
+                          child: Center(
+                            child: Text(
+                              question.pieces[grouping]["text"].toUpperCase(),
+                              style: TextStyle(fontWeight: FontWeight.w800, fontSize: fonteDaLetra),
+                            ),
                           ),
+                          margin: const EdgeInsets.all(20),
                         ),
-                        margin: const EdgeInsets.all(20),
                       ),
               ),
               // height: cardHeight,
@@ -139,8 +120,7 @@ class MultipleChoiceQuestion extends ConsumerWidget {
                 // if (question.pieces[grouping]["image"].isNotEmpty)
                 Navigator.of(context).pushNamed(
                   ImageDetailScreen.routeName,
-                  arguments: DetailScreenArguments(
-                      grouping: grouping, question: question),
+                  arguments: DetailScreenArguments(grouping: grouping, question: question),
                 );
               },
               onPressed: () {
@@ -148,9 +128,7 @@ class MultipleChoiceQuestion extends ConsumerWidget {
                 if (showConfirmButton == false) showConfirmButton = true;
                 // Muda a flag isCorrect com base na resposta correta fornecida pela questão. Se a resposta atual selecionada for a certa,
                 // a flag receberá true. Ao pressionar o botão de confirmar, ela será enviada como parâmetro.
-                question.pieces["correctAnswer"] == index + 1
-                    ? isCorrect = true
-                    : isCorrect = false;
+                question.pieces["correctAnswer"] == index + 1 ? isCorrect = true : isCorrect = false;
                 print('Correto: ${question.pieces["correctAnswer"]}');
                 // setState(() {
                 //   for (int i = 0; i < 3; i++) {
@@ -203,10 +181,8 @@ class MultipleChoiceQuestion extends ConsumerWidget {
     var cobjectIndex = args.cobjectIndex;
     double screenHeight = MediaQuery.of(context).size.height;
     double textCardHeight = 0.0985 * screenHeight;
-    double buttonHeight =
-        48 > screenHeight * 0.0656 ? 48 : screenHeight * 0.0656;
-    print(
-        'COBJECT LIST ID ${cobjectList[0]} e qindex ${cobjectList[0].questions[questionIndex]}');
+    double buttonHeight = 48 > screenHeight * 0.0656 ? 48 : screenHeight * 0.0656;
+    print('COBJECT LIST ID ${cobjectList[0]} e qindex ${cobjectList[0].questions[questionIndex]}');
 
     String imageLink = cobjectList[0].questions[questionIndex].header["image"];
 
@@ -259,9 +235,7 @@ class MultipleChoiceQuestion extends ConsumerWidget {
               },
           ),
         ),
-        linkImage: imageLink.isNotEmpty
-            ? 'https://elesson.com.br/app/library/image/' + imageLink
-            : null,
+        linkImage: imageLink.isNotEmpty ? 'https://elesson.com.br/app/library/image/' + imageLink : null,
         activityScreen: Container(
           child: Wrap(
             alignment: WrapAlignment.center,
@@ -272,19 +246,13 @@ class MultipleChoiceQuestion extends ConsumerWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
                       height: textCardHeight + 32,
                       child: GestureDetector(
-                        onTap: () => playSound(cobjectList[0]
-                            .questions[questionIndex]
-                            .header["sound"]),
+                        onTap: () => playSound(cobjectList[0].questions[questionIndex].header["sound"]),
                         child: Center(
                           child: Text(
-                            cobjectList[0]
-                                .questions[questionIndex]
-                                .header["text"]
-                                .toUpperCase(),
+                            cobjectList[0].questions[questionIndex].header["text"].toUpperCase(),
                             style: TextStyle(
                               color: Colors.black,
                               fontWeight: FontWeight.bold,
@@ -301,35 +269,13 @@ class MultipleChoiceQuestion extends ConsumerWidget {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 0, vertical: 12),
+                      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 12),
                       child: Column(
                         children: [
                           // Monta as escolhas em uma coluna, com base na função que retorna um Widget de piece unitário.
-                          piece(
-                              pieceOrder[0],
-                              context,
-                              watch,
-                              cobjectList[0].questions[questionIndex],
-                              buttonHeight,
-                              screenHeight,
-                              textCardHeight),
-                          piece(
-                              pieceOrder[1],
-                              context,
-                              watch,
-                              cobjectList[0].questions[questionIndex],
-                              buttonHeight,
-                              screenHeight,
-                              textCardHeight),
-                          piece(
-                              pieceOrder[2],
-                              context,
-                              watch,
-                              cobjectList[0].questions[questionIndex],
-                              buttonHeight,
-                              screenHeight,
-                              textCardHeight),
+                          piece(pieceOrder[0], context, watch, cobjectList[0].questions[questionIndex], buttonHeight, screenHeight, textCardHeight),
+                          piece(pieceOrder[1], context, watch, cobjectList[0].questions[questionIndex], buttonHeight, screenHeight, textCardHeight),
+                          piece(pieceOrder[2], context, watch, cobjectList[0].questions[questionIndex], buttonHeight, screenHeight, textCardHeight),
                         ],
                       ),
                     ),
